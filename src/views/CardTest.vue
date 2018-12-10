ab73b2eaceeb4268e77f9ff031b8e4da52b783c5
bravf <bravfing@126.com>
2018-12-10 14:48:11 +0800

    change: 修改 card 样式

--- src/views/CardTest.vue
+++ src/views/CardTest.vue
@@ -1,25 +1,52 @@
 <template lang="pug">
-r-card(
-  style="margin: 10px; width:500px;"
-  title="你好"
-)
-  p(slot="title")
-    | 标准卡片哈哈哈哈哈
-  
-  a(href="#", slot="extra")
-    r-button-group(size="small")
-      r-button| 你好
-      r-button| 再见
-  
+div(style="padding: 20px; background-color: #fff")
   r-card(
-    title="标准卡片"
-  ) 
-    a(href="#", slot="extra")
-      r-icon(type="ios-loop-strong")
-      span(style="vertical-align: middle;")| 换一换
+    title="你好"
+  )
+    p(slot="title")
+      | 标准卡片哈哈哈哈哈
     
-    p| 卡片内容
-    p| 卡片内容
-    p| 卡片内容
+    a(href="#", slot="extra")
+      r-button-group(size="small")
+        r-button| 你好
+        r-button| 再见
+    
+    r-card(
+      title="标准卡片"
+    ) 
+      a(href="#", slot="extra")
+        r-icon(type="ios-loop-strong")
+        span(style="vertical-align: middle;")| 换一换
+      
+      p| 卡片内容
+      p| 卡片内容
+      p| 卡片内容
+  
+
+  r-card(
+    title="你好"
+  )
+    p(slot="title")
+      | 标准卡片哈哈哈哈哈
+    
+    a(href="#", slot="extra")
+      r-button-group(size="small")
+        r-button| 你好
+        r-button| 再见
+    
+    p| 你好呀
+
+  r-card(
+    title="你好"
+  )
+    p(slot="title")
+      | 标准卡片哈哈哈哈哈
+    
+    a(href="#", slot="extra")
+      r-button-group(size="small")
+        r-button| 你好
+        r-button| 再见
+    
+    p| hello, world ~
 
 </template>
